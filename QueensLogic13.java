5e998ba8afc38179aa89e0646d0b32dba4a6a57d
ldej <ldej@itu.dk>
2022-03-20 06:55:56 +0100

    init

---
+++ QueensLogic13.java
@@ -0,0 +1,21 @@
+public class QueensLogic13 implements IQueensLogic {
+
+    @Override
+    public void initializeBoard(int size) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int[][] getBoard() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void insertQueen(int column, int row) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
