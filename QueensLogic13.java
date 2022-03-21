d9e9f3ffccc2645f4539ff584cc0183a7e3fd2be
ldej <ldej@itu.dk>
2022-03-21 05:33:25 +0100

    change apply to withAnd, withOr

--- QueensLogic13.java
+++ QueensLogic13.java
@@ -59,17 +59,17 @@ public class QueensLogic13 implements IQueensLogic {
     //  Placing a queen makes the variable true
     //  excludedRule: conjucntion of negations, since placing a queen on one of the excluded positions will make the conjunction false, and hence we do not end up having a true BDD
     //  oneQueenInEachRowRule: disjunction since at least one variable in the disjunction must be true to make the disjunction true, and that is placing a queen in at least one row, since there is a disjunction for each row.
-    public void buildRules() {
+    public void buildRules() { //INIT RULES
         int nVars = size*size; // The number of variables to be used is the number of squares on the board
 		fact.setVarNum(nVars);
 
         //rule: no other queen can be positioned on the same row, column or any of the diagonals (https://homes.cs.aau.dk/~srba/courses/SV-08/material/09.pdf)
         for (int variable = 0; variable < nVars; variable++) {
-            rules = rules.apply(excludedRule(variable), BDDFactory.and);
+            rules.andWith(excludedRule(variable));//rules = rules.and(excludedRule(variable)); //IS THE SAME rules.apply(excludedRule(variable), BDDFactory.and);
         }
 
         //rule: must be at least one queen in each row (https://homes.cs.aau.dk/~srba/courses/SV-08/material/09.pdf)
-        rules = rules.apply(oneQueenInEachRowRule(), BDDFactory.and);
+            rules.andWith(oneQueenInEachRowRule());//rules = rules.and(oneQueenInEachRowRule()); //IS THE SAME rules.apply(oneQueenInEachRowRule(), BDDFactory.and);
     }
 
     //https://homes.cs.aau.dk/~srba/courses/SV-08/material/09.pdf
@@ -79,9 +79,9 @@ public class QueensLogic13 implements IQueensLogic {
             BDD rowRule = False; //The disjunction identity is false: https://dogedaos.com/wiki/Disjunction.html
 
             for (int col = 0; col < size; col++) { //run through the cols and make the disjunction of the positions in the row: x1 OR x2 OR x3 etc.
-                rowRule.apply(fact.ithVar(getVariable(col, row)), BDDFactory.or);
+                rowRule.orWith(fact.ithVar(getVariable(col, row)));//IS THE SAME apply(fact.ithVar(getVariable(col, row)), BDDFactory.or);
             }
-            rule.apply(rowRule, BDDFactory.and); //conjoin the individual row rules such that row1Rule AND row2Rule AND row3Rule etc.
+            rule.andWith(rowRule); //IS THE SAME apply(rowRule, BDDFactory.and); //conjoin the individual row rules such that row1Rule AND row2Rule AND row3Rule etc.
         }
         return rule;
     }
@@ -90,7 +90,7 @@ public class QueensLogic13 implements IQueensLogic {
     public BDD excludedRule(int variable) { //conjunction
         BDD rule = True; //The conjunctive identity is true //https://en.wikipedia.org/wiki/Logical_conjunction
         for (int excludedVariable : getExcludedVariablesBy(variable)) {
-            rule.apply(fact.nithVar(excludedVariable), BDDFactory.and); //negate all the excluded variables meaning that the excluded variables must be false to make the conjunction true
+            rule.andWith(fact.nithVar(excludedVariable)); //IS THE SAME apply(fact.nithVar(excludedVariable), BDDFactory.and); //negate all the excluded variables meaning that the excluded variables must be false to make the conjunction true
         }
         //if the variable is true (the queen is on the position/variable) the excluded variables must be negated
         return fact.ithVar(variable).imp(rule);
@@ -184,11 +184,11 @@ public class QueensLogic13 implements IQueensLogic {
 
     //RESTRICTIONS
     //returns a BDD that is a conjunction of all the restrictions for the restrictions on the positions where a queen is placed.
-    private BDD getRestrictions() {
+    private BDD getRestrictions() { //ASSIGNING VALUES TO VARIABLES: the ithVar must be true, the nithVar must be false in order to make the BDD true in the end
         BDD restrictions = True; //the identity of conjunction is true
         for (int queenPosition : queenPositions) {
             //Since each variable has a BDD containing the rules for the variable (build when calling buildRules), all the variables/positions where a queen is placed contains the rules, and these rules are "added" together here
-            restrictions.apply(fact.ithVar(queenPosition), BDDFactory.and);
+            restrictions.andWith(fact.ithVar(queenPosition)); //IS THE SAME AS restrictions.apply(fact.ithVar(queenPosition), BDDFactory.and);
         }
         return restrictions;
     }
