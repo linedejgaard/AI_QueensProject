a6877bed9760e1028c0d20f399376a492df78996
jakmum <jakobmummert@gmail.com>
2022-03-24 09:03:57 +0100

    some clean up for submission

--- QueensLogic13.java
+++ QueensLogic13.java
@@ -14,7 +14,6 @@ public class QueensLogic13 implements IQueensLogic {
     BDD False = fact.zero();
 
     BDD rules = True; //this is going to be a conjunction, and since the identity of conjunction is true, this is set to true
-    BDD restrictedBDD = True;
     
     @Override
     public void initializeBoard(int size) {
@@ -44,13 +43,15 @@ public class QueensLogic13 implements IQueensLogic {
     public void updateBoard() {
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
-                BDD pos = rules.restrict(fact.ithVar(getVariable(col, row)));
-                BDD neg = rules.restrict(fact.nithVar(getVariable(col, row)));
-                if (neg.isZero()) { //not inserting a queen here is a contradiction
-                    board[col][row] = 1;
-                    insertQueen(col, row);
-                } else if (pos.isZero()) { //inserting a queen here is a contradiction
-                    board[col][row] = -1;
+                if (board[col][row] == 0) {
+                    BDD pos = rules.restrict(fact.ithVar(getVariable(col, row)));
+                    BDD neg = rules.restrict(fact.nithVar(getVariable(col, row)));
+                    if (neg.isZero()) { //not inserting a queen here is a contradiction
+                        board[col][row] = 1;
+                        insertQueen(col, row);
+                    } else if (pos.isZero()) { //inserting a queen here is a contradiction
+                        board[col][row] = -1;
+                    }
                 }
             }
         }
@@ -65,7 +66,7 @@ public class QueensLogic13 implements IQueensLogic {
         }
     }
 
-    //RULES: //https://homes.cs.aau.dk/~srba/courses/SV-08/material/09.pdf (exclusion of variables and one queen in each row)
+    //RULES: //https://learnit.itu.dk/pluginfile.php/312490/mod_label/intro/a97.pdf (exclusion of variables and one queen in each row)
     //excludedRule: no other queen can be positioned on the same row, column or any of the diagonals
     //oneQueenInEachRowRule: at least one queen in each row: conjunction of disjunctions
     //NOTES:
@@ -77,34 +78,34 @@ public class QueensLogic13 implements IQueensLogic {
         int nVars = size*size; // The number of variables to be used is the number of squares on the board
 		fact.setVarNum(nVars);
 
-        //rule: no other queen can be positioned on the same row, column or any of the diagonals (https://homes.cs.aau.dk/~srba/courses/SV-08/material/09.pdf)
+        //rule: no other queen can be positioned on the same row, column or any of the diagonals (https://learnit.itu.dk/pluginfile.php/312490/mod_label/intro/a97.pdf)
         for (int variable = 0; variable < nVars; variable++) {
-            rules = rules.and(excludedRule(variable));//rules = rules.and(excludedRule(variable)); //IS THE SAME rules.apply(excludedRule(variable), BDDFactory.and);
+            rules = rules.and(excludedRule(variable));
         }
 
-        //rule: must be at least one queen in each row (https://homes.cs.aau.dk/~srba/courses/SV-08/material/09.pdf)
-        rules = rules.and(oneQueenInEachRowRule());//rules = rules.and(oneQueenInEachRowRule()); //IS THE SAME rules.apply(oneQueenInEachRowRule(), BDDFactory.and);
+        //rule: must be at least one queen in each row (https://learnit.itu.dk/pluginfile.php/312490/mod_label/intro/a97.pdf)
+        rules = rules.and(oneQueenInEachRowRule());
     }
 
-    //https://homes.cs.aau.dk/~srba/courses/SV-08/material/09.pdf
+    //https://learnit.itu.dk/pluginfile.php/312490/mod_label/intro/a97.pdf
     public BDD oneQueenInEachRowRule() { //disjunction: this means that there must be at least ONE queen in each row to make the BDD true
         BDD rule = True; //this is the overall rule for all the rows
         for (int row = 0; row < size; row++) { //run through the rows and make a rowRule for each
             BDD rowRule = False; //The disjunction identity is false: https://dogedaos.com/wiki/Disjunction.html
 
             for (int col = 0; col < size; col++) { //run through the cols and make the disjunction of the positions in the row: x1 OR x2 OR x3 etc.
-                rowRule = rowRule.or(fact.ithVar(getVariable(col, row)));//IS THE SAME apply(fact.ithVar(getVariable(col, row)), BDDFactory.or);
+                rowRule = rowRule.or(fact.ithVar(getVariable(col, row)));
             }
-            rule = rule.and(rowRule); //IS THE SAME apply(rowRule, BDDFactory.and); //conjoin the individual row rules such that row1Rule AND row2Rule AND row3Rule etc.
+            rule = rule.and(rowRule); 
         }
         return rule;
     }
 
-    //https://homes.cs.aau.dk/~srba/courses/SV-08/material/09.pdf
+    //https://learnit.itu.dk/pluginfile.php/312490/mod_label/intro/a97.pdf
     public BDD excludedRule(int variable) { //conjunction
         BDD rule = True; //The conjunctive identity is true //https://en.wikipedia.org/wiki/Logical_conjunction
         for (int excludedVariable : getExcludedVariablesBy(variable)) {
-            rule = rule.and(fact.nithVar(excludedVariable)); //IS THE SAME apply(fact.nithVar(excludedVariable), BDDFactory.and); //negate all the excluded variables meaning that the excluded variables must be false to make the conjunction true
+            rule = rule.and(fact.nithVar(excludedVariable)); 
         }
         //if the variable is true (the queen is on the position/variable) the excluded variables must be negated
         return fact.ithVar(variable).imp(rule);
@@ -113,7 +114,7 @@ public class QueensLogic13 implements IQueensLogic {
     //help get methods: lists
     private ArrayList<Integer> getExcludedVariablesBy(int variable) {
         ArrayList<Integer> excludedVariables = getVariablesInSameCol(variable); //exclude variables in same col
-        excludedVariables.addAll(getVariablesInSameRow(variable)); //exclude variables in same col
+        excludedVariables.addAll(getVariablesInSameRow(variable)); //exclude variables in same row
         excludedVariables.addAll(getVariablesDiagonal(variable)); //exclude variables in same dia
 
         return excludedVariables;
@@ -183,7 +184,6 @@ public class QueensLogic13 implements IQueensLogic {
     }
 
     private int getRow(int variable) {
-        //return variable/size; //I don't know if this gives rows. It depends on how divid works on ints, I think...
         return getRowFromCol(variable, getCol(variable));
     }
 
